a3de5cd3fd459d3d76af3c15e958000984ef8994
Alexsandro Thomas <alexsandrogthomas@gmail.com>
2021-03-05 19:03:48 -0300

    Adding decompact

--- app/compact.cxx
+++ app/compact.cxx
@@ -8,8 +8,11 @@
 #include <limits>
 #include <compare>
 #include <bitset>
+#include <cmath>
 
 #include <fire-hpp/fire.hpp>
+
+uint64_t INF = ~(uint64_t(0));
 
 struct Tree {
     std::optional<char> byte;
@@ -30,6 +33,16 @@ struct Cmp {
     }
 };
 
+uint64_t write_tree(std::vector<char>& position, Tree* node, uint64_t index) {
+    if (node->left.get()==NULL) {
+        position[index]=*node->byte;
+        if (*node->byte==0) return index;
+        return INF;
+    }
+
+    return std::min(write_tree(position, node->left.get(), index*2+1), write_tree(position, node->right.get(), index*2+2));
+}
+
 int fired_main(std::string file_path = fire::arg({"--file-path","-f"}), fire::optional<int> print = fire::arg({"--print","-p"})) {
     //open file
     std::ifstream file(file_path, std::fstream::binary);
@@ -45,15 +58,15 @@ int fired_main(std::string file_path = fire::arg({"--file-path","-f"}), fire::op
     //read file + save byte frequency
     std::unordered_map<char,uint64_t> freq;
 
-    std::cout << "ASCII: ";
+    if (print.value_or(1)) std::cout << "ASCII: ";
 
     char buf;
     while (file >> buf) {
-        std::cout << std::bitset<8>(buf);
+        if (print.value_or(1)) std::cout << std::bitset<8>(buf);
         freq[buf]++;
         total_bytes++;
     }
-    std::cout << " (" << total_bytes*8 << ')' << std::endl;
+    if (print.value_or(1)) std::cout << " (" << total_bytes*8 << ')' << std::endl;
 
     //create tree leaves
     std::priority_queue<Tree*, std::vector<Tree*>, Cmp> leaves;
@@ -72,13 +85,17 @@ int fired_main(std::string file_path = fire::arg({"--file-path","-f"}), fire::op
     //traverse the tree and create paths
     std::map<char,std::string> path;
     std::queue<std::pair<Tree*,std::string>> traverse;
-    traverse.push(std::make_pair(leaves.top(),""));
-    leaves.pop();
+    Tree* root = leaves.top(); leaves.pop();
+    traverse.push(std::make_pair(root,""));
+
+    int deepest=0;
 
     while (traverse.size()>0) {
         auto [t,p] = traverse.front(); traverse.pop();
 
         if (t->left.get()==NULL) {
+            if (p.size()>deepest) deepest=p.size();
+
             path[*t->byte]=p;
             total_bits_huffman+=p.size()*freq[*t->byte];
             if (print.value_or(1)) std::cout << *t->byte << ": " << path[*t->byte] << '\n';
@@ -89,16 +106,43 @@ int fired_main(std::string file_path = fire::arg({"--file-path","-f"}), fire::op
         traverse.push(std::make_pair(t->right.get(),p+'1'));
     }
 
-    //print the paths
-    std::cout << "Huffman: ";
+    //create the buffer with 3 extra bits (trailing bits in the last byte)
+    std::ofstream out_file("out.huff", std::fstream::binary);
+    std::bitset<8> binary_buf((8-((total_bits_huffman+3))%8)<<5);
+    char buf_pos=3;
+
+    //print the paths + output to file
+    if (print.value_or(1)) std::cout << "Huffman: ";
 
     file.clear(); file.seekg(0);
     while (file >> buf) {
-        std::cout << path[buf];
+        if (print.value_or(1)) std::cout << path[buf];
+        for (const char c : path[buf]) {
+            binary_buf[7-(buf_pos++)]=c-'0';
+            if (buf_pos==8) {
+                buf_pos=0;
+                out_file << (unsigned char)binary_buf.to_ulong();
+            }
+        }
     }
-    std::cout << " (" << total_bits_huffman << ')' << std::endl;
+    if (buf_pos!=0)
+        out_file << (unsigned char)binary_buf.to_ulong();
+    if (print.value_or(1)) std::cout << " (" << total_bits_huffman << ')' << std::endl;
 
     file.close();
+    out_file.close();
+
+    //write tree
+    std::ofstream out_tree("out.tree", std::fstream::binary);
+    uint64_t tree_size=pow(2,deepest+1)-1;
+    std::vector<char> positions(tree_size,0);
+    uint64_t zeroindex = write_tree(positions, root, 0);
+    out_tree << zeroindex;
+    //std::cout << std::bitset<64>(zeroindex) << '\n';
+    for (char c : positions)
+        out_tree << c;
+    out_file.close();
+
     return 0;
 }
 
---
+++ app/decompact.cxx
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <memory>
+#include <utility>
+#include <map>
+#include <queue>
+#include <limits>
+#include <compare>
+#include <bitset>
+#include <set>
+#include <vector>
+
+#include <fire-hpp/fire.hpp>
+
+struct Tree {
+    char byte;
+    std::unique_ptr<Tree> left, right;
+
+    Tree(Tree* p, char side, char b): byte(b) {
+        if (side) p->right.reset(this);
+        else p->left.reset(this);
+    }
+    Tree(char b): byte(b) {}
+};
+
+int fired_main(std::string file_path = fire::arg({"--file-path","-f"}), std::string tree_path = fire::arg({"--tree","-t"}), fire::optional<int> print = fire::arg({"--print","-p"})) {
+    //open file
+    std::ifstream file(file_path, std::fstream::binary);
+
+    if (!file.good()) {
+        std::cerr << "Couldn't open file!" << std::endl;
+        return 1;
+    }
+    file >> std::noskipws;
+
+    //read tree
+    std::ifstream tree_file(tree_path, std::fstream::binary);
+    if (!file.good()) {
+        std::cerr << "Couldn't open tree!" << std::endl;
+        return 1;
+    }
+    tree_file >> std::noskipws;
+
+    uint64_t zeroindex;
+    tree_file >> zeroindex;
+
+    std::vector<char> tree_positions;
+
+    char buf;
+    while (tree_file >> buf) {
+        tree_positions.push_back(buf);
+    }
+
+    using q_item = std::tuple<Tree*,char,uint64_t>;
+    std::queue<q_item> q;
+
+    Tree* root = new Tree(tree_positions[0]);
+    q.push(q_item(root,0,1));
+    q.push(q_item(root,1,2));
+
+    while (!q.empty()) {
+        auto &[p,side,index] = q.front(); q.pop();
+        Tree* self = new Tree(p,side,tree_positions[index]);
+        if (tree_positions[index]==0 && index!=zeroindex && index*2+2<tree_positions.size()) {
+            q.push(q_item(self,0,index*2+1));
+            q.push(q_item(self,1,index*2+2));
+        }
+    }
+    tree_file.close();
+
+    //read file
+    if (print.value_or(1)) std::cout << "Huffman message: ";
+
+    unsigned char buf_pos=3;
+    file >> buf;
+    std::bitset<8> binary_buf(buf);
+    file >> buf;
+
+    const unsigned char trailing = binary_buf[7]*4+binary_buf[6]*2+binary_buf[5];
+    Tree* traversing = root;
+    std::string original;
+
+    do {
+        while (buf_pos!=8) {
+            if (print.value_or(1)) std::cout << binary_buf[7-buf_pos];
+            if (binary_buf[7-buf_pos])
+                traversing = traversing->right.get();
+            else
+                traversing = traversing->left.get();
+            
+            if (traversing->left.get()==NULL) {
+                original+=traversing->byte;
+                traversing = root;
+            }
+            buf_pos++;
+        }
+        buf_pos=0;
+        binary_buf = std::bitset<8>(buf);
+    } while (file >> buf);
+
+    while (buf_pos!=8-trailing) {
+        if (print.value_or(1)) std::cout << binary_buf[7-buf_pos];
+        if (binary_buf[7-buf_pos])
+            traversing = traversing->right.get();
+        else
+            traversing = traversing->left.get();
+        
+        if (traversing->left.get()==NULL) {
+            original+=traversing->byte;
+            traversing = root;
+        }
+        buf_pos++;
+    }
+    if (print.value_or(1)) std::cout << std::endl;
+
+    file.close();
+
+    if (print.value_or(1)) std::cout << "Original message: " << original << std::flush;
+
+    //write tree
+    std::ofstream out_plain("out.plain", std::fstream::binary);
+    out_plain << original;
+    out_plain.close();
+
+    return 0;
+}
+
+FIRE(fired_main, "Calculate file entropy")
